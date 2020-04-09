224651c71a7dfdf5b4580b7709642757c4853c4c
rtkleong10 <rtkleong10@gmail.com>
2020-04-09 18:37:11 +0800

    [update] Add login required and require get to web home

--- web/views.py
+++ web/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
+@login_required
+@require_http_methods(["GET"])
 def home_view(request):
 	return render(request, 'web/home.html')
