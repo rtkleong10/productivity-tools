345bbf4831204354b4bc9fd318c08e394abd0471
rtkleong10 <rtkleong10@gmail.com>
2020-07-13 17:46:01 +0800

    [update] Renamed ProfileApi to ProfileView

--- common/urls.py
+++ common/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from .apis import ColorViewSet, TimezoneViewSet, DefaultRouterWithSimpleViews
-from registration.apis import ProfileApi
 from days_since.apis import ActivityViewSet, ActivityEventViewSet
+from registration.apis import ProfileView
 
 router = DefaultRouterWithSimpleViews()
 router.register(r'colors', ColorViewSet, basename='colors')
 router.register(r'timezones', TimezoneViewSet, basename='timezones')
+router.register(r'profile', ProfileView, basename='profile')
 router.register(r'activities', ActivityViewSet, basename='activity')
-router.register(r'profile', ProfileApi, basename='profile')
 router.register(r'activities/(?P<activity>[^/.]+)/events', ActivityEventViewSet, basename='activityevent')
 
 urlpatterns = [
