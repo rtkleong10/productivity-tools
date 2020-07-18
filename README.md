418be59e79110b1a2d24a65b7bf0acdec4b9695e
rtkleong10 <rtkleong10@gmail.com>
2020-07-18 17:05:24 +0800

    [update] Initialise time cycles

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 - It provides API endpoints for the productivity tools web app
 
 ## Tools
-- Days Since (incomplete)
-- Habit Tracker (not started)
-- Progress Bar (not started)
+- Days Since
+- Time Cycles
     
 ### Days Since
 - It allows you to track when you've last completed an activity, so you know whether you need to do it again soon
+
+### Time Cycles
+- It allows you to create sequences of timers to performed timed tasks
 
 ## Resources
 - Favicon is a modified version of a [Flaticon icon](https://www.flaticon.com/free-icon/clock_2784459)
--- common/urls.py
+++ common/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .apis import ColorViewSet, TimezoneViewSet, DefaultRouterWithSimpleViews
 from registration.apis import ProfileView
 from days_since.apis import ActivityViewSet, ActivityEventViewSet
+from time_cycles.apis import CycleViewSet, TimerViewSet
 
 router = DefaultRouterWithSimpleViews()
 router.register(r'colors', ColorViewSet, basename='colors')
@@ -11,6 +12,10 @@ router.register(r'profile', ProfileView, basename='profile')
 # Days Since
 router.register(r'activities', ActivityViewSet, basename='activity')
 router.register(r'activities/(?P<activity>[^/.]+)/events', ActivityEventViewSet, basename='activityevent')
+
+# Time Cycles
+router.register(r'cycles', CycleViewSet, basename='cycle')
+router.register(r'cycles/(?P<cycle>[^/.]+)/timers', TimerViewSet, basename='timer')
 
 urlpatterns = [
     path('', include((router.urls, 'api'), namespace='api')),
